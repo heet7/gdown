afeacdf8764cf51672c082fa3218582da1ce31d7
heet bhalala <44464602+heet7@users.noreply.github.com>
2021-08-23 18:41:30 +0530

    Update gdown.py

--- gdown.py
+++ gdown.py
@@ -1,1 +1,9 @@
+# get the shared link of data
+# https://drive.google.com/uc?id=1ImZGCZ9TF_xIjw7n693qOo8VXTJXbAIJ
+# then gat id and pest it hear
+!gdown --id 1ImZGCZ9TF_xIjw7n693qOo8VXTJXbAIJ
 
+# and store it in url
+url = 'https://drive.google.com/uc?id=1ImZGCZ9TF_xIjw7n693qOo8VXTJXbAIJ'
+# and you can access url 
+data = pd.read_csv(url)
